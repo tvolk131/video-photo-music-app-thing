b3b2f21272f43be49fb4a5d35005a960576316cf
Spencer Mitchell <smitch.work@gmail.com>
2017-09-02 09:39:48 -0700

    Setup client test suite

---
+++ test/client/components/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import App from '../../../client/src/App.js';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+test('<App />', () => {
+  it('renders \'Hello World!\'', () => {
+    const App = shallow(<App />);
+    expect(App.find('h2').text()).toBe('Hello World!');
+  });
+});
