472b6e0665ed087f5018a369ac25b19f3f4f0656
Spencer Mitchell <smitch.work@gmail.com>
2017-09-05 14:47:02 -0700

    Setup build and connect react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Hello World!</h2>
         </div>
+        
       </div>
     );
   }
---
+++ client/src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App.jsx';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render((
+  <BrowserRouter basename="/">
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
+
+registerServiceWorker();
