161829c242428e32eaedd00a538832189d37478d
Spencer Mitchell <smitch.work@gmail.com>
2017-09-06 10:33:40 -0700

    Implement routes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './redux/store.js';
 import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <BrowserRouter basename="/">
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
+  </Provider>
+    
+  
 ), document.getElementById('root'));
 
 registerServiceWorker();
