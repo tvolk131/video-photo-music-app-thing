c5060cdc1006bc38eaaaffcae6c6a952632c4d26
Spencer Mitchell <smitch.work@gmail.com>
2017-09-06 11:00:56 -0700

    Add bare layouts

---
+++ client/src/layouts/Search.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Search layout page</h2>
+      </div>
+    );
+  }
+}
+
+export default Search;
