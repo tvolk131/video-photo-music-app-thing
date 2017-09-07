19a5e3d0ecadc7a2f79fc99b82a6fc884859d769
Spencer Mitchell <smitch.work@gmail.com>
2017-09-07 02:10:15 -0700

    Remove redundant imports

--- client/src/layouts/Search.jsx
+++ client/src/layouts/Search.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Grid from 'material-ui/Grid';
-import Paper from 'material-ui/Paper';
-import Card, { CardContent, CardMedia } from 'material-ui/Card';
 import SearchResults from '../containers/SearchResults.jsx';
 
 class Search extends Component {
