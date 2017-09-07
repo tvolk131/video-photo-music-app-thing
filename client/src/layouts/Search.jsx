1ca3e72f71bbbc2ef09b6d0b702cc0d621564106
Spencer Mitchell <smitch.work@gmail.com>
2017-09-07 01:54:51 -0700

    Add SearchResults rendering in Search layout

--- client/src/layouts/Search.jsx
+++ client/src/layouts/Search.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
+import Card, { CardContent, CardMedia } from 'material-ui/Card';
+import SearchResults from '../containers/SearchResults.jsx';
 
 class Search extends Component {
   render() {
     return (
-      <div>
-        <h2>Search layout page</h2>
+      <div style={{padding: '2%'}}>
+        <SearchResults />   
       </div>
     );
   }
