92e78eadbeee33fcbc6b4894fa918de8d68449b3
Spencer Mitchell <smitch.work@gmail.com>
2017-09-07 20:22:54 -0700

    Add initial state data for testing

--- client/src/redux/reducers/data.js
+++ client/src/redux/reducers/data.js
@@ -1,39 +1,68 @@
 const initialState = {
+  currentProject: {
+    owner: {
+      name: 'Some guy',
+      avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
+      jobTitle: 'Web Developer'
+    },
+    contributors: [
+      {
+        name: 'Some guy',
+        avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
+        jobTitle: 'Web Developer'
+      },
+      {
+        name: 'Some guy',
+        avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
+        jobTitle: 'Web Developer'
+      },
+      {
+        name: 'Some guy',
+        avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
+        jobTitle: 'Web Developer'
+      },
+      {
+        name: 'Some guy',
+        avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
+        jobTitle: 'Web Developer'
+      },
+    ]
+  },
   searchResults: [
     {
       name: 'Some cool project',
-      thumbnailUrl: 'http://via.placeholder.com/300x300',
+      thumbnailUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
       owner: {
         name: 'John Doe',
         username: 'XxXjohndoeXxX420',
-        avatarUrl: 'http://via.placeholder.com/300x300'
+        avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg'
       }
     },
     {
       name: 'Some cool project',
-      thumbnailUrl: 'http://via.placeholder.com/300x300',
+      thumbnailUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
       owner: {
         name: 'John Doe',
         username: 'XxXjohndoeXxX420',
-        avatarUrl: 'http://via.placeholder.com/300x300'
+        avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg'
       }
     },
     {
       name: 'Some cool project',
-      thumbnailUrl: 'http://via.placeholder.com/300x300',
+      thumbnailUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
       owner: {
         name: 'John Doe',
         username: 'XxXjohndoeXxX420',
-        avatarUrl: 'http://via.placeholder.com/300x300'
+        avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg'
       }
     },
     {
       name: 'Some cool project',
-      thumbnailUrl: 'http://via.placeholder.com/300x300',
+      thumbnailUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg',
       owner: {
         name: 'John Doe',
         username: 'XxXjohndoeXxX420',
-        avatarUrl: 'http://via.placeholder.com/300x300'
+        avatarUrl: 'https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg'
       }
     }
   ]
