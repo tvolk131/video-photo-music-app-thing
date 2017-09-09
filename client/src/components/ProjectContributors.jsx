031cce8f98e3d1d143282f0ac6d992c56fd57186
Brett Kirk <brett_kirk@me.com>
2017-09-08 21:12:16 -0700

    Dynamically render components on project page

---
+++ client/src/components/ProjectContributors.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+ 
+import Avatar from 'material-ui/Avatar';
+import Paper from 'material-ui/Paper';
+import Card, { CardContent, CardMedia, CardHeader } from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
+import List, { ListItem, ListItemText, ListItemAvatar } from 'material-ui/List';
+import Typography from 'material-ui/Typography';
+ 
+const ProjectContributors = ({ owner, contributors }) => {
+   
+  return (
+    <Card>
+      <CardHeader
+        style={{backgroundColor: '#3F51B5', fontColor: 'white'}}
+        title={<Typography style={{color: 'white'}}>Contributors</Typography>}
+      />
+      <CardHeader
+        avatar={
+          <Avatar src={owner.avatarUrl} style={{width: 50, height: 50}}>
+             
+          </Avatar>
+        }
+        title={owner.name}
+        subheader='Project Owner'
+        style={{textAlign: 'left'}}
+      />
+      <Divider />
+ 
+      <List dense>
+        {contributors.map((contributor, key) => {
+          var key = key++ || 0;
+          return (
+            <ListItem button key={key}>
+              <ListItemAvatar>
+                <Avatar src={contributor.avatarUrl}></Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={contributor.name}
+                secondary={contributor.jobTitle}
+                style={{textAlign: 'left'}}
+              />
+              <Divider inset />
+            </ListItem>
+          );
+        })}
+      </List>
+    </Card>
+  );
+};
+ 
+export default ProjectContributors; 
