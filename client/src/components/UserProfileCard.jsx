871f62efe4f386de21259ad2fa284a0b1bebdf3d
Spencer Mitchell <smitch.work@gmail.com>
2017-09-09 17:28:05 -0700

    Implement UserProfileCard

---
+++ client/src/components/UserProfileCard.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { match } from 'react-router';
+
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import Card, { CardContent, CardMedia, CardTitle } from 'material-ui/Card';
+import { CircularProgress } from 'material-ui/Progress';
+import Typography from 'material-ui/Typography';
+
+import { gql, graphql } from 'react-apollo';
+
+class UserProfileCard extends Component {
+
+  render() {
+
+    if (this.props.data.loading) {
+      return (
+        <Paper style={{padding: 25}}>
+          <CircularProgress/>
+        </Paper>
+      );
+    } else {
+
+      const {
+        avatarUrl,
+        name,
+        username,
+        description
+      } = this.props.data.user;
+    
+      return (
+        <Paper>
+          <img src='https://s3-us-west-1.amazonaws.com/qraft-image-files/stock+male+1.jpeg' style={{width: '100%'}}/>
+          <div style={{textAlign: 'left', padding: 10}}>
+            <h3>{username}</h3>
+            <Typography>{username}</Typography>
+            <Typography>Bio:</Typography>
+            <Typography>{description}</Typography>
+          </div>
+        </Paper>
+      );
+    }
+  }
+}
+
+const userQuery = gql`
+  query userProjectsQuery($username: String!) {
+    user(username: $username) {
+      id
+      name
+      username
+      description
+      avatarUrl
+    }
+  }
+`;
+
+export default graphql(userQuery, {
+  options: ({ username }) => ({variables: { username }})
+})(UserProfileCard);
