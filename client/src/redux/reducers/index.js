b2a5b01d5cdf03f91a0ce1225fec0003c7be510e
Spencer Mitchell <smitch.work@gmail.com>
2017-09-09 17:17:22 -0700

    Add session reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import control from './control';
 import data from './data';
+import session from './session';
 
 const reducer = combineReducers({
   control,
+  session,
   data
 });
 
