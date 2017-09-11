020bd03c31c84fc394bebe875572e4c8e72b9cc0
Thomas Volk <tvolk131@gmail.com>
2017-09-10 23:27:20 -0700

    Has more mock data and imports environment variables correctly

--- db/mock/index.js
+++ db/mock/index.js
@@ -1,52 +1,70 @@
 let mockData = require('./mockData.json');
 let db = require('../');
 
-db.connection.reset()
-  .catch(() => {
-    setTimeout(() => {
+const injectMockData = () => {
+  db.connection.reset()
+    .catch(() => {
+      setTimeout(injectMockData, 500);
+    })
+    .then(() => {
+      let promises = [];
+      mockData.users.forEach((userObj) => {
+        promises.push(
+          db.User.create(userObj)
+        );
+      });
+      return Promise.all(promises);
+    })
+    .then(() => {
+      let promises = [];
+      mockData.projects.forEach((projectObj) => {
+        promises.push(
+          db.Project.create(projectObj)
+        );
+      });
+      return Promise.all(promises);
+    })
+    .then(() => {
+      let promises = [];
+      mockData.projectContributors.forEach((projectContributorObj) => {
+        promises.push(
+          db.Project.addContributor(projectContributorObj)
+        );
+      });
+      return Promise.all(promises);
+    })
+    .then(() => {
+      let promises = [];
+      mockData.projectComponents.forEach((projectComponentObj) => {
+        promises.push(
+          db.ProjectComponent.create(projectComponentObj)
+        );
+      });
+      return Promise.all(promises);
+    })
+    .then(() => {
+      let promises = [];
+      mockData.projectComments.forEach((projectCommentObj) => {
+        promises.push(
+          db.Project.Comment.create(projectCommentObj)
+        );
+      });
+      return Promise.all(promises);
+    })
+    .then(() => {
+      let promises = [];
+      mockData.projectComponentComments.forEach((projectComponentCommentObj) => {
+        promises.push(
+          db.ProjectComponent.Comment.create(projectComponentCommentObj)
+        );
+      });
+      return Promise.all(promises);
+    })
+    .then(() => {
       console.log();
       console.log();
-      console.log('Whoops, something went wrong when generating mock data. Try running the script again.');
-    }, 1000);
-  })
-  .then(() => {
-    let promises = [];
-    mockData.users.forEach((userObj) => {
-      promises.push(
-        db.User.create(userObj)
-      );
+      console.log('Mock data has been injected!');
     });
-    return Promise.all(promises);
-  })
-  .then(() => {
-    let promises = [];
-    mockData.projects.forEach((projectObj) => {
-      promises.push(
-        db.Project.create(projectObj)
-      );
-    });
-    return Promise.all(promises);
-  })
-  .then(() => {
-    let promises = [];
-    mockData.projectContributors.forEach((projectContributorObj) => {
-      promises.push(
-        db.Project.addContributor(projectContributorObj)
-      );
-    });
-    return Promise.all(promises);
-  })
-  .then(() => {
-    let promises = [];
-    mockData.projectComponents.forEach((projectComponentObj) => {
-      promises.push(
-        db.ProjectComponent.create(projectComponentObj)
-      );
-    });
-    return Promise.all(promises);
-  })
-  .then(() => {
-    console.log();
-    console.log();
-    console.log('Mock data has been injected!');
-  });
+}
+
+injectMockData();
