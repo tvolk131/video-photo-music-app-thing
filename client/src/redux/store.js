a0631549ce5c91b8795fc217a6ef3f35e0d7000f
Spencer Mitchell <smitch.work@gmail.com>
2017-09-11 00:20:32 -0700

    Apply axios middleware

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import axiosMiddleware from 'redux-axios-middleware';
+import axios from 'axios';
 import reducer from './reducers';
+
+const client = axios.create({
+  baseURL:'http://localhost:3000/',
+  responseType: 'json'
+});
 
 const store = createStore(
   reducer,
+  applyMiddleware(axiosMiddleware),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
