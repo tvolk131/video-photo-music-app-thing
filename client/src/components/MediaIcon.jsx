cfe8730ed88292907e9edd430c75c189a1a045b9
Spencer Mitchell <smitch.work@gmail.com>
2017-09-12 09:12:38 -0700

    Refactor MediaIcon out of ListingItem

---
+++ client/src/components/MediaIcon.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+ 
+import ImageIcon from 'material-ui-icons/Image';
+import AudiotrackIcon from 'material-ui-icons/Audiotrack';
+import TextFieldsIcon from 'material-ui-icons/TextFields';
+import TheatersIcon from 'material-ui-icons/Theaters';
+import List, { ListItem, ListItemText, ListItemAvatar } from 'material-ui/List';
+
+const MediaIcon = ({ type }) => {
+  const style = {
+    height: 30,
+    width: 30,
+    margin: 5,
+    margin: 0
+  };
+ 
+  if (type === 'text') {
+    return (<TextFieldsIcon style={style} />);
+
+  } else if (type === 'audio') {
+    return (<AudiotrackIcon style={style} />);
+
+  } else if (type === 'image') {
+    return (<ImageIcon style={style} />);
+
+  } else if (type === 'video') {
+    return (<TheatersIcon style={style} />);
+  }
+};
+
+export default MediaIcon;
