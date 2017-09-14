7af681ce08dc7b6fb22afcb6ef95afb00a750ced
Spencer Mitchell <smitch.work@gmail.com>
2017-09-13 20:13:57 -0700

    Remove redundant redux store provider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 
 import store from './redux/store.js';
 import './index.css';
@@ -9,11 +8,9 @@ import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <Provider store={store}>
-    <BrowserRouter basename="/">
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter basename="/">
+    <App />
+  </BrowserRouter>
 ), document.getElementById('root'));
 
-registerServiceWorker();
+registerServiceWorker.register();
