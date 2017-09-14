904f432f0bb5210be9dd0155c8d59fec88b38527
Spencer Mitchell <smitch.work@gmail.com>
2017-09-14 14:35:33 -0700

    Refactor tests with new file paths

--- test/client/App.test.jsx
+++ test/client/App.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-import App from '../../../client/src/App.jsx';
+import App from '../../client/src/App.jsx';
 
 test('<App />', () => {
   
