75bd603d80efa076becd1a07e776c6f208f98e43
Brett Kirk <brett_kirk@me.com>
2017-09-15 11:38:21 -0700

    Start edit user card, factor out loading circle, improve upload dropzone

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,39 +1,54 @@
 import React from 'react';
 import Grid from 'material-ui/Grid';
-import Paper from 'material-ui/Paper';
-import Card, { CardContent, CardMedia } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
-import AddIcon from 'material-ui-icons/Add';
+import { CircularProgress } from 'material-ui/Progress';
 
-const Upload = () => {
+const Upload = ({ allowedType }) => {
   const dropzoneStyles = { 
-    marginBottom: 30,
     border: 'dashed', 
     borderColor: 'grey', 
     borderRadius: 10, 
     width: '100%', 
-    height: 200
+    height: 250,
+    overflow: 'hidden',
+    maxWidth: '100%',
+    boxSizing: 'border-box'
   };
+  let dropzoneFileTypes = [];
 
-  let dropzoneFileTypes = [
-  //IMAGE FILE TYPES
-    'image/png',
-    'image/jpg',
-    'image/gif',
-    'image/bmp',
-    'image/svg',
-    //VIDEO FILE TYPES
-    'video/mp4',
-    'video/webm',
-    //TEXT FILE TYPES
-    'text/plain',
-    //AUDIO FILE TYPES
-    'audio/mp3',
-    'audio/wav',
-    'audio/ogg',
-    'audio/webm'
-  ];
+  if (allowedType === 'image') {
+    dropzoneFileTypes = [
+      'image/png',
+      'image/jpeg',
+      'image/gif',
+      'image/bmp',
+      'image/svg+xml'
+    ];
+  } else {
+    dropzoneFileTypes = [
+      //IMAGE FILE TYPES
+      'image/png',
+      'image/jpg',
+      'image/gif',
+      'image/bmp',
+      'image/svg+xml',
+      //VIDEO FILE TYPES
+      'video/mp4',
+      'video/webm',
+      //TEXT FILE TYPES
+      'text/plain',
+      //AUDIO FILE TYPES
+      'audio/mp3',
+      'audio/wav',
+      'audio/ogg',
+      'audio/webm',
+      'audio/aac'
+    ];
+  }
+
+  const imageComponent = (({ uploadedFile }) => <img src={uploadedFile.fileUrl} style={{objectFit: 'cover', width: 'auto', height: '100%', align: 'center'}}/>);
+  const progressComponent = (({progress}) => (progress ? (<div style={{margin: 50}}><CircularProgress size={150} mode="determinate" value={progress} min={0} max={25} /></div>) : null));
 
   dropzoneFileTypes = dropzoneFileTypes.join(',');
 
@@ -42,27 +57,33 @@ const Upload = () => {
     console.log('the file link is ' + info.fileUrl);
   };
 
+
   const uploadOptions = {
     server: 'http://' + window.location.host
   };
   const s3Url = 'https://qraft-uploads.s3.amazonaws.com';
 
+  console.log(dropzoneStyles);
+  console.log(dropzoneFileTypes);
+  console.log(handleFinishedUpload);
+  console.log(s3Url);
+  console.log(uploadOptions);
+
   return (
-    <Grid container spacing={24}>
-      <Grid item sm />
-      <Grid item xs={12} sm={10} md={8}>
-        <Card style={{display: 'flex'}}>
-          <Grid container spacing={0} justify="center">
-            <Grid item xs={12}>
-              <div style={{width: '100%', margin: 15}}>Drag files from desktop or click to upload.</div>
-            </Grid>
-            <Grid item xs={10}>
-              <DropzoneS3Uploader style={dropzoneStyles} accept={dropzoneFileTypes} onFinish={handleFinishedUpload} s3Url={s3Url} upload={uploadOptions} />
-            </Grid>
-          </Grid>
-        </Card>
+    <Grid container spacing={0} justify="center">
+      <Grid item xs={11}>
+        <DropzoneS3Uploader 
+          style={dropzoneStyles} 
+          accept={dropzoneFileTypes} 
+          onFinish={handleFinishedUpload} 
+          s3Url={s3Url} 
+          upload={uploadOptions} 
+          imageComponent={imageComponent}
+          progressComponent={progressComponent} />
       </Grid>
-      <Grid item sm></Grid>
+      <Grid item xs={12}>
+        <Typography style={{width: '100%', margin: 5}}>Drag files from desktop or click to upload.</Typography>
+      </Grid>
     </Grid>
   );
 };
