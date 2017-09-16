ecfcc6ea24cd47c17db0f6424219a5033f1b9a9c
Thomas Volk <tvolk131@gmail.com>
2017-09-16 15:12:08 -0700

    Server indexes users into elasticsearch

--- db/elasticSearchControllers/user.js
+++ db/elasticSearchControllers/user.js
@@ -1,1 +1,20 @@
-module.exports = require('../models/user');
+const elasticSearch = require('../../elasticSearch');
+const dbUser = require('../models/user');
+
+let User = {...dbUser};
+
+User.create = (input) => {
+  return dbUser.create(input)
+    .tap((user) => {
+      elasticSearch.indexUser(user);
+    });
+};
+
+User.update = (userId, query) => {
+  return dbUser.update(userId, query)
+    .tap((user) => {
+      elasticSearch.indexUser(user);
+    });
+};
+
+module.exports = User;
