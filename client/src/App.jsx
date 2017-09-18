f52edddf72aa598fca6658838a9090c61af581d3
Spencer Mitchell <smitch.work@gmail.com>
2017-09-18 11:02:06 -0700

    Implement create project view and move Apollo Provider out of App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router';
-
-import { client } from './reducers';
-import store from './store.js';
+import { connect } from 'react-redux';
 
 import Nav from './containers/Nav';
 import Search from './layouts/Search.jsx';
@@ -11,6 +9,7 @@ import User from './layouts/User.jsx';
 import Settings from './layouts/Settings.jsx';
 import Login from './layouts/Login.jsx';
 import Signup from './layouts/Signup.jsx';
+import CreateProject from './components/CreateProject.jsx';
 
 import themes from './themes';
 
@@ -19,30 +18,24 @@ import { MuiThemeProvider } from 'material-ui/styles';
 import logo from './logo.svg';
 import './App.css';
 
-import {
-  ApolloClient,
-  ApolloProvider
-} from 'react-apollo';
-
 const App = () => (
-  <ApolloProvider client={client} store={store}>
-    <MuiThemeProvider theme={themes.get(0)}>
-      <div className="App">
-        <Nav />
-        <Route exact path='/' component={Search}/>
-        <Route exact path='/search' component={Search}/>
-        <Route
-          exact
-          path='/project/:username/:projectName'
-          component={Project}
-        />
-        <Route exact path='/user/:username' component={User}/>
-        <Route exact path='/settings' component={Settings}/>
-        <Route exact path='/login' component={Login}/>
-        <Route exact path='/signup' component={Signup}/>
-      </div>
-    </MuiThemeProvider>
-  </ApolloProvider>
+  <MuiThemeProvider theme={themes.get(0)}>
+    <div className="App">
+      <Nav />
+      <Route exact path='/' component={Search}/>
+      <Route exact path='/search' component={Search}/>
+      <Route
+        exact
+        path='/project/:username/:projectName'
+        component={Project}
+      />
+      <Route exact path='/project/create' component={CreateProject}/>
+      <Route exact path='/user/:username' component={User}/>
+      <Route exact path='/settings' component={Settings}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/signup' component={Signup}/>
+    </div>
+  </MuiThemeProvider>
 );
 
 export default App;
