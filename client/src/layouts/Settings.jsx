2788a9d97369c7ba4cc9a9b671018b8ae17278b2
Spencer Mitchell <smitch.work@gmail.com>
2017-09-18 16:56:18 -0700

    Reimplement theme changing

--- client/src/layouts/Settings.jsx
+++ client/src/layouts/Settings.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { gql, graphql } from 'react-apollo';
+
 import PasswordChanger from '../components/settings_components/PasswordChanger.jsx';
 import ThemeChanger from '../components/settings_components/ThemeChanger.jsx';
 
@@ -15,20 +18,38 @@ const styles = {
   }
 };
 
-class Settings extends Component {
-  render() {
-    return (
-      <div style={styles.wrapper}>
-        <h2>Settings</h2>
-        <div style={styles.section}>
-          <PasswordChanger />
-        </div>
-        <div style={styles.section}>
-          <ThemeChanger />
-        </div>
-      </div>
-    );
-  }
-}
+const propTypes = {
+  setTheme: PropTypes.func.isRequired
+};
 
-export default Settings;
+const Settings = ({ setTheme }) => (
+  <div style={styles.wrapper}>
+    <h2>Settings</h2>
+    <div style={styles.section}>
+      <PasswordChanger />
+    </div>
+    <div style={styles.section}>
+      <ThemeChanger
+        setTheme={setTheme}
+      />
+    </div>
+  </div>
+);
+
+const setTheme = gql`
+  mutation editUser($themeId: Int!) {
+    editUser(theme: $themeId) {
+      theme
+    }
+  }
+`;
+
+Settings.propTypes = propTypes;
+
+export default graphql(setTheme, {
+  props: ({ ownProps, mutate }) => ({
+    setTheme(themeId) {
+      mutate({variables: {themeId}});
+    }
+  })
+})(Settings);
