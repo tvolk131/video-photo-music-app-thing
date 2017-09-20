a4f622825556805998e205cdb7b28d60439e5337
Spencer Mitchell <smitch.work@gmail.com>
2017-09-20 11:26:01 -0700

    Resize thumbnail and add divider

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -26,70 +26,72 @@ const ListingItem = ({ content }) => {
   };
   
   return (
-    <Grid container spacing={0}>
-      <Route render={({history}) => (
-        <ListItem
-          button
-          onClick={() => {
-            let path = '/search';
-            if (content.username) {
-              path = `/user/${content.username}`;
-            } else if (content.owner) {
-              path = `/project/${content.owner.username}/${content.name}`;
-            } else if (content.author) {
-              console.log('no such path yet');
-            }
-            history.push(path);
-          }}
-          style={{width: '100%', padding: 0}}
-        > 
-          <Grid container>
-            <Grid item xs={3} style={{
-              marginTop: 'auto',
-              marginBottom: 'auto',
-              align: 'right',
-              padding: 0
-            }}>
-              <img
-                src={content.thumbnailUrl || content.avatarUrl} 
-                style={{
-                  height: 50,
-                  width: 50
-                }}
-              />
-            </Grid>
-            <Grid item style={{textAlign: 'left'}}>
-              <h4 style={{marginBottom: 0}}>{content.name}</h4>
-              <Grid
-                container
-                align='center'
-                spacing={0}
-                style={{
-                  marginTop: 0,
-                  marginBottom: 'auto'
-                }}
-              >
-                {!content.username &&
-                  <div>
-                    <Grid item>
-                      <Avatar
-                        src={content.author ? content.author.avatarUrl : content.owner.avatarUrl}
-                        style={smallIcon}
-                      />
-                    </Grid>
-                    <Grid item>
-                      {content.author ? content.author.name : content.owner.name}
-                    </Grid>
-                  </div>
-                }
+    <div>
+      <Grid container spacing={0}>
+        <Route render={({history}) => (
+          <ListItem
+            button
+            onClick={() => {
+              let path = '/search';
+              if (content.username) {
+                path = `/user/${content.username}`;
+              } else if (content.owner) {
+                path = `/project/${content.owner.username}/${content.name}`;
+              } else if (content.author) {
+                console.log('no such path yet');
+              }
+              history.push(path);
+            }}
+            style={{width: '100%', padding: 0}}
+          > 
+            <Grid container>
+              <Grid item xs={3} style={{
+                marginTop: 'auto',
+                marginBottom: 'auto',
+                align: 'right',
+                padding: 0
+              }}>
+                <img
+                  src={content.thumbnailUrl || content.avatarUrl} 
+                  style={{
+                    height: 75,
+                    width: 75
+                  }}
+                />
               </Grid>
+              <Grid item style={{textAlign: 'left'}}>
+                <h4 style={{marginBottom: 0}}>{content.name}</h4>
+                <Grid
+                  container
+                  align='center'
+                  spacing={0}
+                  style={{
+                    marginTop: 0,
+                    marginBottom: 'auto'
+                  }}
+                >
+                  {!content.username &&
+                    <div>
+                      <Grid item>
+                        <Avatar
+                          src={content.author ? content.author.avatarUrl : content.owner.avatarUrl}
+                          style={smallIcon}
+                        />
+                      </Grid>
+                      <Grid item>
+                        {content.author ? content.author.name : content.owner.name}
+                      </Grid>
+                    </div>
+                  }
+                </Grid>
+              </Grid>
+              <SocialButtons likeCount={randomCount()}/>
             </Grid>
-            <SocialButtons likeCount={randomCount()}/>
-          </Grid>
-        </ListItem> 
-      )} />
+          </ListItem> 
+        )} />
+        </Grid>
       <Divider/>
-    </Grid>
+    </div>
   );
 };
 
